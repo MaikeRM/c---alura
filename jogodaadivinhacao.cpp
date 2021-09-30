98de29a4cd142c584705b785a61dd891a508a9f2
MaikeRM <maikemota@live.com>
2021-09-30 16:09:19 -0300

    first commit

---
+++ jogodaadivinhacao.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+
+int main(){
+    sdt::cout << "*************************************\n";
+    sdt::cout << "* Bem vindos ao jogo da adivinhação \n*";
+    sdt::cout << "*************************************\n";
+
+}
+
+
